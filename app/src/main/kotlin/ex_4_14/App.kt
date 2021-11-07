1008cfdbd3fdf7d5d5bbd0b3a90983bac81e97d7
Denis Manikhin <dmanikhine@gmail.com>
2021-11-07 19:07:36 +0100

    Final commit

--- app/src/main/kotlin/ex_4_14/App.kt
+++ app/src/main/kotlin/ex_4_14/App.kt
@@ -3,13 +3,27 @@
  */
 package ex_4_14
 
+
+fun <T> unfold(seed: T, f: (T) -> T, p: (T) -> Boolean): List<T> {
+    tailrec fun unfold_(acc: List<T>, seed: T): List<T> =
+            if (p(seed))
+                unfold_(acc + seed, f(seed))
+            else
+                acc
+    return unfold_(listOf(), seed)
+}
+
+fun range(start: Int, end: Int): List<Int> = unfold(start, { it + 1 }, { it <=end })
+
 class App {
     val greeting: String
         get() {
-            return "Hello World!"
+            return "package ex_4_14"
         }
 }
 
 fun main() {
     println(App().greeting)
+	println(range(1,10))
+	
 }
